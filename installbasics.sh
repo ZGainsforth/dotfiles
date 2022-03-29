845da2fa3d16855f75a67e01050fbe70c26c454a
Zack Gainsforth <zsg@gainsforth.com>
2022-03-28 23:39:56 -0700

    installbasics

--- installbasics.sh
+++ installbasics.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-sudo apt install zsh vim-gtk autojump
+sudo apt install zsh vim-gtk autojump kitty git tmux npm
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k  
-
+cp vimrcgit ../.vimrc
+cp tmux.confgit ../.tmux.conf
+cp zshrcgit ../.zshrc
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# npm install -g tldr
 
 echo Install anaconda from website.
